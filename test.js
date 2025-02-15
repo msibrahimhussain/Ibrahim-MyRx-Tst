a8c1144af30708c88fe8221dfc755759ec0e525d
msibrahimhussain <141616747+msibrahimhussain@users.noreply.github.com>
2025-02-15 16:08:39 +0530

    Update test.js

--- test.js
+++ test.js
@@ -44,11 +44,6 @@ function minPlatforms(arr, dep) {
     }
     return maxPlatforms;
 }
-//
-
-//5
-
-
 
 console.log(minPlatforms(["9:00", "9:40", "9:50", "11:00", "15:00", "18:00"], 
                          ["9:10", "12:00", "11:20", "11:30", "19:00", "20:00"])); 
